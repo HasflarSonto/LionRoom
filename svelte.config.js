abf8488416949a98112ad767633b2b1c5492708c
Patrick Shen <patbshen@gmail.com>
2025-03-28 14:45:37 -0400

    changed nodejs18 to nodejs18.x

--- svelte.config.js
+++ svelte.config.js
@@ -7,7 +7,7 @@ const config = {
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter({
-			runtime: 'nodejs18'  // Use Node.js runtime instead of edge
+			runtime: 'nodejs18.x'  // Changed from 'nodejs18' to 'nodejs18.x'
 		}),
 		alias: {
 			$data: 'public/data'
